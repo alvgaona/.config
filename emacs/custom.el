10550edf3d8c99c7fa6ce33e962e7376ec0a0644
Alvaro Gaona <alvgaona@gmail.com>
2026-01-04 12:36:16 +0100

    Add emacs config

---
+++ emacs/custom.el
@@ -0,0 +1,22 @@
+;;; -*- lexical-binding: t -*-
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(ag clojure-mode cmake-mode d-mode dash-functional diminish
+        dockerfile-mode elpy glsl-mode go-mode graphviz-dot-mode
+        gruber-darker-theme haskell-mode helm-ls-git
+        ido-completing-read+ jinja2-mode kotlin-mode lua-mode magit
+        markdown-mode move-text multiple-cursors nginx-mode nim-mode
+        nix-mode org-cliplink paredit php-mode powershell
+        proof-general purescript-mode qml-mode racket-mode rfc-mode
+        rust-mode scala-mode smex sml-mode tide toml-mode tuareg
+        typescript-mode uxntal-mode yaml-mode)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
